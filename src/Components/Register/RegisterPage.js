bcd0f09cdb84cce6b7530ae59fe7a4f9a0d49607
Robson Novato <rnlobao42@gmail.com>
2023-09-21 19:51:49 -0300

    começo de validação

--- src/Components/Register/RegisterPage.js
+++ src/Components/Register/RegisterPage.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import validator from 'validator';
 import './RegisterPage.css';
 
 function RegisterPage() {
+
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  const handleEmailChange = (event) => {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+
+    if (!validator.isEmail(newEmail)) {
+      setEmailError('Email inválido');
+    } else {
+      setEmailError('Email inválido');
+    }
+  };
+
   return (
     <div className="register-container">
       <img className='register-girlSearching' src="./Images/register-boy.svg" alt="Imagem" />
@@ -12,11 +28,16 @@ function RegisterPage() {
           <span class="register-metade-esquerda">Já é usuário?</span>
           <span class="register-metade-direita">Faça login</span>
         </h2>
+        
         <input type="text" class="register-nome-textField" placeholder="Nome:"></input>
-        <input type="password" class="register-email-textField" placeholder="Email:"></input>
+        <input type="text" class="register-email-textField" placeholder="Email:" value={email} onChange={handleEmailChange}></input>
+        <h1 className='register-email-erro'>{emailError}</h1>
         <input type="text" class="register-password-textField" placeholder="Senha:"></input>
+        {/* <h1 className='register-nome-erro'>Erro</h1> */}
         <input type="text" class="register-cnpj-textField" placeholder="CNPJ do escritório:"></input>
+        {/* <h1 className='register-nome-erro'>Erro</h1> */}
         <input type="text" class="register-telefone-textField" placeholder="Telefone:"></input>
+        {/* <h1 className='register-nome-erro'>Erro</h1> */}
 
         <div className='register-camposDeEndereco1'>
             <input type="text" class="register-password-textField" placeholder="Logradouro:"></input>
